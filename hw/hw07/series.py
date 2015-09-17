e47e325587b4fdf86eaab64215d39accae702166
Kyle R <KyleRichVA@gmail.com>
2015-09-17 11:19:56 -0700

    finished fibonacci sequence function

--- hw/hw07/series.py
+++ hw/hw07/series.py
@@ -5,6 +5,10 @@ def fibonacci(n):
     Example: fibonacci(0) == 0
              fibonacci(1) == 1
     """
-
-    return 1
-    # dummy value
+    # Base values of the sequence, prevents negative number function calls.
+    if(n == 0):
+        return 0
+    if(n == 1):
+        return 1
+    # Returns the sum of the previous two values in the series.
+    return fibonacci(n-1) + fibonacci(n-2)
