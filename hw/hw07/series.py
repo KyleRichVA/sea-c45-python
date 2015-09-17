6794be4e4a9ca4b70178b2bfa8c26aa935f6673e
Kyle R <KyleRichVA@gmail.com>
2015-09-16 20:49:05 -0700

    Created series.py, started fibonacci function

---
+++ hw/hw07/series.py
@@ -0,0 +1,10 @@
+def fibonacci(n):
+    """
+    Returns the nth number from the fibonacci sequence.
+
+    Example: fibonacci(0) == 0
+             fibonacci(1) == 1
+    """
+
+    return 1
+    # dummy value
