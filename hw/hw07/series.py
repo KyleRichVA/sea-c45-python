4d6fac202f1dd2b7b76f3b39cdec85312cce8b28
Kyle R <KyleRichVA@gmail.com>
2015-09-18 11:58:21 -0700

    Started lucas function, added negative number protection

--- hw/hw07/series.py
+++ hw/hw07/series.py
@@ -10,5 +10,20 @@ def fibonacci(n):
         return 0
     if(n == 1):
         return 1
+
     # Returns the sum of the previous two values in the series.
     return fibonacci(n-1) + fibonacci(n-2)
+
+
+def lucas(n):
+    """
+    returns the nth number from the lucas numbers series
+    """
+    # Base values of the series
+    if (n == 0):
+        return 2
+    if (n== 1):
+        return 1
+
+    # returns the sum of the previous two values in the series.
+    return lucas(n-1) + lucas(n-2) 
