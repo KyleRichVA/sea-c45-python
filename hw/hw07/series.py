63f79194c8e38ff6f6872a36eebdd359f2a7863d
Kyle R <KyleRichVA@gmail.com>
2015-09-19 23:15:54 -0700

    modified formating to fit with more flake8 stuff

--- hw/hw07/series.py
+++ hw/hw07/series.py
@@ -14,7 +14,7 @@ def fibonacci(n):
         return 1
 
     # Returns the sum of the previous two values in the series.
-    return fibonacci(n-1) + fibonacci(n-2)
+    return fibonacci(n - 1) + fibonacci(n - 2)
 
 
 def lucas(n):
@@ -30,12 +30,13 @@ def lucas(n):
         return 1
 
     # returns the sum of the previous two values in the series.
-    return lucas(n-1) + lucas(n-2)
+    return lucas(n - 1) + lucas(n - 2)
 
 
 def sum_series(n, first=0, second=1):
     """
-    Returns the nth number from a sum series starting with the 'first' and 'second' values
+    Returns the nth number from a sum series starting with the 'first' and
+    'second' values
     Example: sum_series(3,5,6) == 11, sum_series(3,1,3) == 4
     """
     # Base values of the series
@@ -46,5 +47,5 @@ def sum_series(n, first=0, second=1):
     if(n == 1):
         return second
 
-    #returns the sum of the previous two values in the series.
-    return sum_series(n-1, first, second) + sum_series(n-2, first, second)
+    # returns the sum of the previous two values in the series.
+    return sum_series(n - 1, first, second) + sum_series(n - 2, first, second)
