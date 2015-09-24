e156333c38f29bb4ced09437497a402fd9b298a6
Kyle R <KyleRichVA@gmail.com>
2015-09-24 12:36:46 -0700

    Completed first version of thank_you() personal testing looks good

--- hw/hw11/mailroom.py
+++ hw/hw11/mailroom.py
@@ -7,8 +7,6 @@ send pre-created thank you letter to new or previous
 donors.
 """
 
-import string
-
 # global values to make the dictonary calls more human readable
 TOTAL_DONATED = 0
 NUM_DONATIONS = 1
@@ -34,15 +32,45 @@ def thank_you():
     print(u"list - Print a list of previous donors")
     print(u"quit - Return to main menu")
     name_input = input(u"> ")
+    # return to main menu if quit is entered
+    if(name_input.lower() == "quit"):
+        return
+    name_input = name_input.title()
+
     print(u"Please enter a donation amount or 'quit':")
     # value validation
     while True:
         try:
             amount_input = input(u"> ")
+            # return to main menu if quit is entered
+            if(amount_input.lower() == "quit"):
+                return
             amount_float = float(amount_input)
             break
         except ValueError:
-            print(u"Please type a valid dollar amount. (no $ symbol)")
+            print(u"Please type a valid numeric dollar amount. (no symbols)")
 
+    # if the donor is already in the database modify their info
+    if(name_input in donors.keys()):
+        donors[name_input][TOTAL_DONATED] += amount_float
+        donors[name_input][NUM_DONATIONS] += 1
+    # otherwise create a new value in the dictonary
+    else:
+        donors[name_input] = [amount_float, 1]
 
+    # print out the custom thank you letter
+    print(u"Dear {},\n".format(name_input))
+    print(u"We would like to thank you for your generous donation of {}"
+          " dollars to the Richardson Foundation. These funds will go a long"
+          " way in the fight aganist Ice Wurm infections\n"
+          .format(amount_input))
+    print(u"Thank You,\n\nKyle Richardson\n\nThe Richardson Foundation"
+          "\n\nPress Enter to Continue...\n")
+    input(">")
 
+    # return to starting prompt
+    return
+
+# TESTING
+thank_you()
+print(donors)
