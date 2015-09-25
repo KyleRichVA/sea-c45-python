3d373ba68792b42253c9bca93cfe27f9f8ffac1c
Kyle R <KyleRichVA@gmail.com>
2015-09-25 14:29:43 -0700

    Started early line and word gathering and trigram creation

---
+++ hw/hw13/trigram.py
@@ -0,0 +1,23 @@
+# get the file used to create the trigram
+file = open("sherlock_small.txt", "r")
+# take the text and stores in memory line by line
+lines = []
+for line in file:
+    lines.append(file.readline())
+words = []
+trigrams = {}
+
+for line in lines:
+    words += line.replace('--', ' ').replace('\n', ' ').replace('.', '')\
+                 .replace(',', '').split()
+for i, word in enumerate(words):
+    # if are able to get 3 words without a index out of range.
+    if(i + 1 in range(len(words)) and i + 2 in range(len(words))):
+        # if a two word sequence already appeared.
+        if((word, words[i + 1]) in trigrams.keys()):
+            trigrams[(word, words[i + 1])].append(words[i + 2])
+        # otherwise create a new trigram for that sequence
+        else:
+            trigrams[(word, words[i + 1])] = [words[i + 2]]
+
+print(trigrams)
