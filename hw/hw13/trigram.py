68174f7c89080a091a31ac61930aa3e47355387c
Kyle R <KyleRichVA@gmail.com>
2015-09-27 09:47:50 -0700

    testing out text modification

--- hw/hw13/trigram.py
+++ hw/hw13/trigram.py
@@ -1,7 +1,7 @@
 import random
 # get the file used to create the trigram
 file = open("sherlock_small.txt", "r")
-# take the text and stores in memory line by line
+# take the text and store in memory line by line
 lines = []
 for line in file:
     lines.append(file.readline())
@@ -9,8 +9,7 @@ words = []
 trigrams = {}
 
 for line in lines:
-    words += line.replace('--', ' ').replace('\n', ' ').replace('.', '')\
-                 .replace(',', '').split()
+    words += line.replace('--', ' ').replace('\n', ' ').split()
 for i, word in enumerate(words):
     # if are able to get 3 words without a index out of range.
     if(i + 1 in range(len(words)) and i + 2 in range(len(words))):
@@ -22,7 +21,7 @@ for i, word in enumerate(words):
             trigrams[(word, words[i + 1])] = [words[i + 2]]
 
 start = ('as', 'I')
-print(start[0], start[1], trigrams[start][0])
+print(start[0], start[1], trigrams[start][1])
 next = (start[1], trigrams[start][0])
 print(trigrams[next][0])
 
