32ce4daf48b398a1a0ee4fc6efbdca7cbecc8a5b
Kyle R <KyleRichVA@gmail.com>
2015-09-28 12:05:22 -0700

    Commenting and formating, added txt file to repo

--- hw/hw13/trigram.py
+++ hw/hw13/trigram.py
@@ -1,3 +1,8 @@
+"""
+Short Program That Generates Over 100 Words Of A Moby Dick
+Style Story Using Trigrams.
+"""
+
 import random
 # get the file used to create the trigram
 file = open("mobydick.txt", "r")
@@ -5,11 +10,16 @@ file = open("mobydick.txt", "r")
 lines = []
 for line in file:
     lines.append(file.readline())
+file.close()
+
+# Used to store individual words and trigrams.
 words = []
 trigrams = {}
 
+# Go through each line and split it up into indivdual words
 for line in lines:
     words += line.replace('--', ' ').replace('\n', '').split(' ')
+# Go through each word and set up a trigram (word, nextword): wordafternextword
 for i, word in enumerate(words):
     # if are able to get 3 words without a index out of range.
     if(i + 1 in range(len(words)) and i + 2 in range(len(words))):
@@ -19,9 +29,11 @@ for i, word in enumerate(words):
         # otherwise create a new trigram for that sequence
         else:
             trigrams[(word, words[i + 1])] = [words[i + 2]]
+
 # the random starting trigram and printing for the text generation.
 start = random.choice(list(trigrams.keys()))
 print(start[0], start[1], trigrams[start][0], end=' ')
+# set up the next trigram key
 nextKey = (start[1], trigrams[start][0])
 lastWord = start[1]
 # generates new text while the the last two words printed exist in the trigram
@@ -33,6 +45,7 @@ while(nextKey in trigrams.keys() and wordsPrinted < wordLimit):
     ran = random.choice(range(len(trigrams[nextKey])))
     newWord = trigrams[nextKey][ran]
     print(newWord, end=' ')
+    # set up the next trigram
     lastWord = nextKey[1]
     nextKey = (lastWord, newWord)
     wordsPrinted += 1
