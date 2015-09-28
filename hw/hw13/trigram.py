076317be73c92c6225d6eeccbc71f8257445f5a2
Kyle R <KyleRichVA@gmail.com>
2015-09-27 17:06:58 -0700

    got text generation working for sherlock test file.

--- hw/hw13/trigram.py
+++ hw/hw13/trigram.py
@@ -9,7 +9,7 @@ words = []
 trigrams = {}
 
 for line in lines:
-    words += line.replace('--', ' ').replace('\n', ' ').split()
+    words += line.replace('--', ' ').replace('\n', '').split(' ')
 for i, word in enumerate(words):
     # if are able to get 3 words without a index out of range.
     if(i + 1 in range(len(words)) and i + 2 in range(len(words))):
@@ -19,9 +19,14 @@ for i, word in enumerate(words):
         # otherwise create a new trigram for that sequence
         else:
             trigrams[(word, words[i + 1])] = [words[i + 2]]
-
-start = ('as', 'I')
-print(start[0], start[1], trigrams[start][1])
-next = (start[1], trigrams[start][0])
-print(trigrams[next][0])
-
+# start with a random trigram.
+start = ("as", "I")
+print(start[0], start[1], trigrams[start][0], end=' ')
+nextKey = (start[1], trigrams[start][0])
+lastWord = start[1]
+while(nextKey in trigrams.keys()):
+    ran = random.choice(range(len(trigrams[nextKey])))
+    newWord = trigrams[nextKey][ran]
+    print(newWord, end=' ')
+    lastWord = nextKey[1]
+    nextKey = (lastWord, newWord)
