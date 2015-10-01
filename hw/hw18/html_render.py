dc2a0d55e2bd63a2a5cd9155f9d4b1fc3f0eccdd
Kyle R <KyleRichVA@gmail.com>
2015-10-01 13:46:25 -0700

    Working on part 1

--- hw/hw18/html_render.py
+++ hw/hw18/html_render.py
@@ -7,5 +7,20 @@ Python class example.
 
 # The start of it all:
 # Fill it all in here.
+
+
 class Element(object):
-    pass
+
+    def __init__(self, name=""):
+        self.name = name
+        self.contains = []
+        self.indent = None
+
+    def append(self, content):
+        self.contains.append(content)
+
+    def render(self, file_out, indent=""):
+        file_out.write("<html>\n")
+        for values in self.contains:
+            file_out.write(values)
+        file_out.write("\n</html>")
