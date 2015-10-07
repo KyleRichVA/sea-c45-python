50d8d771bf4fe8fbcf54d53e331225f15a890664
Kyle R <KyleRichVA@gmail.com>
2015-10-07 11:19:28 -0700

    added test_get_words() which is working

--- hw/hw13/test_trigram.py
+++ hw/hw13/test_trigram.py
@@ -4,3 +4,10 @@ import trigram
 def test_read_lines():
     lines = trigram.readLines("sherlock_small.txt")
     assert(len(lines))
+
+
+def test_get_words():
+    lines = ["hey everyone\n", "these are words"]
+    expected = ["hey", "everyone", "these", "are", "words"]
+    words = trigram.getWords(lines)
+    assert words == expected
